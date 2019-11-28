3f95bd60f3545d1e12fbac36ada134f5a60dc4cf
Monica Djunaidi <modjun12@gmail.com>
2019-11-27 19:49:50 -0600

    refactored code and added projects

---
+++ src/javascripts/components/projects/projects.js
@@ -0,0 +1,54 @@
+import utilities from '../../helpers/utilities';
+import projectsData from '../../helpers/data/projectsData';
+
+const createProjectCards = () => {
+  let domString = '';
+  projectsData.getProjects()
+    .then((projects) => {
+      projects.forEach((project) => {
+        domString += `
+        <div class='projectCard col-md-2'>
+            <h3>${project.name}</h3>
+            <a href="${project.url}">Look at this</a>
+            <p>${project.description}</p>
+            <p>${project.technologiesUsed}</p>
+            <a href="${project.url}">Click here!</a>
+            <a href="${project.gitHubUrl}">Check out my Git Hub</a>
+        </div>
+        `;
+        utilities.printToDOM(domString, 'projectsPage');
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+const hidden = () => {
+  document.getElementById('bioPage').style.display = 'none';
+  document.getElementById('technologiesPage').style.display = 'none';
+  document.getElementById('projectsPage').style.display = 'none';
+};
+
+hidden();
+
+const navClick = (e) => {
+  const buttonID = e.target.id;
+  if (buttonID === 'navToBio') {
+    document.getElementById('bioPage').style.display = 'block';
+    document.getElementById('technologiesPage').style.display = 'none';
+    document.getElementById('projectsPage').style.display = 'none';
+  } else if (buttonID === 'navToTechnologies') {
+    document.getElementById('bioPage').style.display = 'none';
+    document.getElementById('technologiesPage').style.display = 'block';
+    document.getElementById('projectsPage').style.display = 'none';
+  } else if (buttonID === 'navToProjects') {
+    document.getElementById('bioPage').style.display = 'none';
+    document.getElementById('technologiesPage').style.display = 'none';
+    document.getElementById('projectsPage').style.display = 'block';
+  }
+};
+
+document.getElementById('navToBio').addEventListener('click', navClick);
+document.getElementById('navToTechnologies').addEventListener('click', navClick);
+document.getElementById('navToProjects').addEventListener('click', navClick);
+
+export default { createProjectCards };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,68 +1,9 @@
 import '../styles/main.scss';
 
-const PrintToDOM = (toPrint, divID) => {
-  document.getElementById(divID).innerHTML = toPrint;
+import projects from './components/projects/projects';
+
+const init = () => {
+  projects.createProjectCards();
 };
 
-const projects = [
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  }];
-
-const createProjectCards = (stuff) => {
-  let domString = '';
-  for (let i = 0; i < stuff.length; i += 1) {
-    const singleThing = stuff[i];
-    if (singleThing.available === true) {
-      domString += `
-    <div class='projectCard'>
-        <h3>${singleThing.title}</h3>
-        <a href="${singleThing.screenshot}">Look at this</a>
-        <p>${singleThing.description}</p>
-        <p>${singleThing.technologiesUsed}</p>
-        <a href="${singleThing.url}">Click here!</a>
-        <a href="${singleThing.githubUrl}">Check out my Git Hub</a>
-    </div>
-    `;
-      PrintToDOM(domString, 'projectsPage');
-    }
-  }
-};
-
-createProjectCards(projects);
-
-const hidden = () => {
-  document.getElementById('bioPage').style.display = 'none';
-  document.getElementById('technologiesPage').style.display = 'none';
-  document.getElementById('projectsPage').style.display = 'none';
-};
-
-hidden();
-
-const navClick = (e) => {
-  const buttonID = e.target.id;
-  if (buttonID === 'navToBio') {
-    document.getElementById('bioPage').style.display = 'block';
-    document.getElementById('technologiesPage').style.display = 'none';
-    document.getElementById('projectsPage').style.display = 'none';
-  } else if (buttonID === 'navToTechnologies') {
-    document.getElementById('bioPage').style.display = 'none';
-    document.getElementById('technologiesPage').style.display = 'block';
-    document.getElementById('projectsPage').style.display = 'none';
-  } else if (buttonID === 'navToProjects') {
-    document.getElementById('bioPage').style.display = 'none';
-    document.getElementById('technologiesPage').style.display = 'none';
-    document.getElementById('projectsPage').style.display = 'block';
-  }
-};
-
-document.getElementById('navToBio').addEventListener('click', navClick);
-document.getElementById('navToTechnologies').addEventListener('click', navClick);
-document.getElementById('navToProjects').addEventListener('click', navClick);
+init();
