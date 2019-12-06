38dbf7cde9cea9778231be7f685a0e0ea130a7d7
Monica Djunaidi <modjun12@gmail.com>
2019-12-05 19:47:51 -0600

    removed navbar component

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -23,33 +23,4 @@ const createProjectCards = () => {
     .catch((error) => console.error(error));
 };
 
-const hidden = () => {
-  document.getElementById('bioPage').style.display = 'none';
-  document.getElementById('technologiesPage').style.display = 'none';
-  document.getElementById('projectsPage').style.display = 'none';
-};
-
-hidden();
-
-const navClick = (e) => {
-  const buttonID = e.target.id;
-  if (buttonID === 'navToBio') {
-    document.getElementById('bioPage').style.display = 'block';
-    document.getElementById('technologiesPage').style.display = 'none';
-    document.getElementById('projectsPage').style.display = 'none';
-  } else if (buttonID === 'navToTechnologies') {
-    document.getElementById('bioPage').style.display = 'none';
-    document.getElementById('technologiesPage').style.display = 'block';
-    document.getElementById('projectsPage').style.display = 'none';
-  } else if (buttonID === 'navToProjects') {
-    document.getElementById('bioPage').style.display = 'none';
-    document.getElementById('technologiesPage').style.display = 'none';
-    document.getElementById('projectsPage').style.display = 'block';
-  }
-};
-
-document.getElementById('navToBio').addEventListener('click', navClick);
-document.getElementById('navToTechnologies').addEventListener('click', navClick);
-document.getElementById('navToProjects').addEventListener('click', navClick);
-
 export default { createProjectCards };
